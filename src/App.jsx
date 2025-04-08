35f28496be55fef26083354cbb5f170c09f01f2e
JEFERSON RODRIGUES DA SILVA <jeferson.rsilva9@senacsp.edu.br>
2025-04-07 22:13:32 -0300

    manipulação dos arquivos app.jsx,Header.jsx e Header.module

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
 import { Contato } from "./pages/Contato"
-import { Route, Router, Routes } from "react-router-dom"
+
 
 function App(){
 
@@ -12,7 +12,7 @@ function App(){
         <Header />
           <Routes>
             <Route path="/" element={<></>} />
-            <Route patch="/cantato" element={<Contato />}/>
+            <Route patch="/contato" element={<Contato />}/>
           </Routes>
         <Footer />
       </Router>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
-import styles from "./HeaderFooter.module.css"
-export function Header(){
-    return(
-        <>
-        <h1>Header</h1>
-        </>
-    )
+import { Link } from "react-router-dom";
+import styles from "./HeaderFooter.module.css";
+
+export function Header() {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.innerHeader}>
+          <h2 className={styles.brand}>Pearson Hardman</h2>
+          <nav className={styles.navbar}>
+            <Link to="/">Início</Link>
+            <Link to="/servicos">Serviços</Link>
+            <Link to="/sobre">Sobre</Link>
+            <Link to="/time">Time</Link>
+            <Link to="/contato">Contato</Link>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
 }
